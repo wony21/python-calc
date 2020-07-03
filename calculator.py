6de0dd349f4b77972f2c28951f68256655912a28
최재원 <njaewon@nate.com>
2020-07-03 17:16:21 +0900

    최초 생성

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# 계산기 모듈
+
+# 더하기
+def add(a, b):
+    return a + b
+# 빼기
+def sub(a, b):
+    return a - b
+# 나누기
+def mul(a, b):
+    return a * b
+# 곱하기
+def div(a, b):
+    return a / b
+# 제곱승
+def sqr(a, b):
+    return a ** b
---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import calculator as calc
+
+# 사용자 값 입력 
+a = input('첫 번째 값을 입력 하세요 :')
+b = input('두 번째 값을 입력 하세요:')
+oper = input('연산자를 입력 하세요:')
+
+# 숫자형인지 판단, 숫자형이 아닌 경우 종료
+if not a.isdecimal() :
+    print('첫 번째 값이 숫자가 아닙니다.', a)
+    sys.exit(1)
+
+if not b.isdecimal():
+    print('두 번째 값이 숫자가 아닙니다.', b)
+    sys.exit(1)
+
+# 형 변환
+a, b = float(a), float(b)
+
+# 해당되는 연산자에 맞게 연산
+if oper == '+':
+    ret = calc.add(a, b)
+elif oper == '-':
+    ret = calc.sub(a, b)
+elif oper == '*':
+    ret = calc.mul(a, b)
+elif oper == '/':
+    ret = calc.div(a, b)
+elif oper == '^':
+    ret = calc.sqr(a, b)
+else:
+    print('잘못된 연산자를 입력 하였습니다.')
+    sys.exit(1)
+
+# 최종 결과 출력
+print('결과 :', a, oper, b, '=', ret)
+
